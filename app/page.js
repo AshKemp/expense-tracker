4a0c215871e087de148e28e72080fabc2770bc62
AshKemp <kmuraliashwin@gmail.com>
2024-06-23 17:46:03 +0530

    1. install button component from shadcn 2. use outfit google font

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Welcome to Expense Tracker App</div>;
+  return (
+    <div>
+      <h2>Welcome to Expense Tracker App</h2>
+      <Button variant="outline">Home Page</Button>
+    </div>
+  );
 }
